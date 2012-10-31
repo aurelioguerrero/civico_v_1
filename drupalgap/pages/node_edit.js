67a39605b2b8ec0c4947259ff1d0e7e145f09057
Aurelio Guerrero <aurelio.guerrero@gmail.com>
2012-10-30 21:29:36 -0500

    opcion camara 11

--- drupalgap/pages/node_edit.js
+++ drupalgap/pages/node_edit.js
@@ -166,7 +166,7 @@ $('#drupalgap_page_node_edit_delete').live('click',function(){
 });
 
 $('#btn_tomar_foto').live('click',function(){
-	navigator.camera.getPicture(mostrarFoto, onFail, { quality: 50/*, destinationType: Camera.destinationType.DATA_URL*/ });
+	navigator.camera.getPicture(mostrarFoto, onFail, { quality: 50, destinationType: navigator.camera.destinationType.DATA_URL });
 });
 
 function mostrarFoto(dato)
