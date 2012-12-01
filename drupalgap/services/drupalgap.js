ece71c741705edd77ec1898c315d9d69dad81f1d
Aurelio Guerrero <aurelio.guerrero@gmail.com>
2012-12-01 12:35:42 -0500

    validacion cuenta siec6

--- drupalgap/services/drupalgap.js
+++ drupalgap/services/drupalgap.js
@@ -300,6 +300,7 @@ var drupalgap_services_drupalgap_user_register = {
 			data += '&mail=' + encodeURIComponent(caller_options.mail);
 			data += '&cuenta=' + encodeURIComponent(caller_options.cuenta);
 			data += '&pass=' + encodeURIComponent(caller_options.pass);
+			alert(data);
 			//, "save_to_local_storage":"0"
 			options = {
 				"resource_path":this.resource_path,
