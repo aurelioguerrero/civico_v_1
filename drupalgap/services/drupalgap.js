4481d1e5ef1e22b024530ea85d979735ae79559c
Aurelio Guerrero <aurelio.guerrero@gmail.com>
2012-12-05 20:51:18 -0500

    se organiza el campo field_cuenta3

--- drupalgap/services/drupalgap.js
+++ drupalgap/services/drupalgap.js
@@ -299,7 +299,7 @@ var drupalgap_services_drupalgap_user_register = {
 			data = 'name=' + encodeURIComponent(caller_options.name);
 			data += '&mail=' + encodeURIComponent(caller_options.mail);			
 			data += '&pass=' + encodeURIComponent(caller_options.pass);
-			data += '&field_cuenta[und][0][value]=' + encodeURIComponent(caller_options.cuenta);
+			data += '&cuenta[field_cuenta][und][0][value]=' + encodeURIComponent(caller_options.cuenta);
 			//, "save_to_local_storage":"0"
 			options = {
 				"resource_path":this.resource_path,
